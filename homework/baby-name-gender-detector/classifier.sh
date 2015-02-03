7609b7efc9a898427d83b28ca6e4f28338c03c5c
Katharine Schwab <kschwab@corn33.stanford.edu>
2015-02-02 21:38:56 -0800

    baby-name-gender-detector

---
+++ homework/baby-name-gender-detector/classifier.sh
@@ -0,0 +1,43 @@
+if [[ $# -eq 0  ]];
+then
+	echo 'Please pass in at least one name.'
+else
+
+for name in "$@"; do 
+datafile='data-hold/namesample.txt'
+name_matches=$(cat $datafile | grep "$name,")
+m_count=0
+f_count=0
+
+for row in $name_matches; do
+	babies=$(echo $row | cut -d ',' -f '3')
+	if [[ $row =~ ',M' ]]
+		then
+			m_count=$((m_count + babies))
+		else
+			f_count=$((f_count + babies))
+	fi
+done
+
+total_babies=$((m_count + f_count))
+
+
+if [[ $total_babies -eq 0 ]]; then
+	echo '$name,NA,0,0'
+else
+pct_female=$((100 * f_count / total_babies))
+pct_male=$((100 * m_count / total_babies))
+
+if [[ pct_female -ge 50 ]]; then
+	g_and_pct="F,$pct_female"
+else 
+	g_and_pct="M,$pct_male"
+fi
+
+echo "$name,$g_and_pct,$total_babies"
+
+fi
+
+done
+
+fi
